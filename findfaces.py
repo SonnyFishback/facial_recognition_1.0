2a3626910750df3c872156a2a70a6c8e87408210
SonnyFishback <sonny.fishback7@gmail.com>
2020-12-05 11:35:22 -0500

    adding facematch.py file and updating findfaces.py file

--- findfaces.py
+++ findfaces.py
@@ -1,3 +1,11 @@
+# This code is used to scan an image and gather the locations of each face that is recognized as a face (known or unknown).
 import face_recognition
 
-image = face_recognition.load_image_file("")
+image = face_recognition.load_image_file("./images/groups/sonnyAndGroup.PNG")
+# Get location of every face recognized in selected image.
+face_locations = face_recognition.face_locations(image)
+# Print array of coordinates for each face.
+# print(face_locations)
+# Print how many faces there are in selected image.
+print(f"There are {len(face_locations)} faces recognized in the selected image.")
+
